6464dd51d024dbe40598123c6d3ce314dc15c004
Professor X <amhester@anderson.edu>
2017-03-11 15:26:42 -0500

    added a simple react/redux ui for color changing grid

---
+++ ui/color-changer/src/App.actions.js
@@ -0,0 +1,2 @@
+export function changeColor(id, color) { return { type: 'CHANGE_COLOR', id, color } };
+export function updateGridSize(width, height) { return { type: 'UPDATE_GRID', width, height } };
